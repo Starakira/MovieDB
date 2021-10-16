7f0632990f113c37e4bf3b05858a8398f097580e
Starakira <bangunagung1@gmail.com>
2021-10-16 22:30:04 +0700

    Initial commit

---
+++ app/src/main/java/com/lab/moviedb/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lab.moviedb.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lab.moviedb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
